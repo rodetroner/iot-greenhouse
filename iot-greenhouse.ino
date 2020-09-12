dcbf9383f5bc39557c2defd6805c50d0781b7dfa
Szymon Hajnos <rodetroner@gmail.com>
2020-09-12 19:14:14 +0200

    Connect the ESP to WiFi

--- iot-greenhouse.ino
+++ iot-greenhouse.ino
@@ -1,5 +1,22 @@
+#include <ESP8266WiFi.h>
+#include "credentials.h"
 
-void setup() {
+void setup()
+{
+	Serial.begin(115200);
+	Serial.println();
+
+	WiFi.begin(ssid, password);
+
+	Serial.print("Connecting");
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print('.');
+	}
+	Serial.println();
+
+	Serial.print("Connected, IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 void loop() {
